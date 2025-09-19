ebf7233fafb2ca6dc84734fdaaeb9b833fccf8aa
Jokel8 <jkl8@web.de>
2025-09-19 18:51:38 +0200

    added text to speech

---
+++ textToSpeech/textToSpeech.py
@@ -0,0 +1,25 @@
+import pyttsx3
+import sys
+
+
+def say(text):
+    try:
+        if type(text) is not str:
+            raise ValueError("Die Eingabe muss ein String sein.")
+
+        engine = pyttsx3.init()
+        engine.setProperty('rate', 200)
+        engine.say(text)
+        engine.runAndWait()
+    except Exception as e:
+        print(f"Fehler bei der Sprachausgabe: {e}")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        text = sys.argv[1]
+    else:
+        text = "Hallo, wie geht es dir? Ich hoffe, du hast einen großartigen Tag!"
+        
+    say(text)
+    print("Sprachausgabe abgeschlossen.")
